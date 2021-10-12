ed2405422015bc26fb6806995034e4aff5e39ca3
Hiter Harris <hiterharris@comcast.net>
2021-10-12 18:37:13 -0400

    render movie list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
 import './App.css';
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    axios.get('https://api.themoviedb.org/3/movie/popular?api_key=727ef961992c940d049df733d71e6951&language=en-US&page=1')
+    .then(res => {
+      const data = res.data.results
+      setMovies(data)
+    })
+  }, [0])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {movies.map((movie) => {
+        return (
+          <div className='movie'>
+              <h3>{movie.title}</h3>
+              <img 
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt="poster"
+                className='poster'
+              />
+          </div>
+        )
+      })}
     </div>
   );
 }
